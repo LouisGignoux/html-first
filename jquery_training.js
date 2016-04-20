ebcb7940482069d1adac45a9404c05dff162b9c2
louisg <louisg@theodo.fr>
2016-04-20 10:56:47 +0200

    Ajout d'un premier script basique jquery

---
+++ jquery_training.js
@@ -0,0 +1,9 @@
+alert('Bienvenue sur le site de la NASA !');
+$(document).ready(function(){
+    $("button").click(function(){$
+        $("header").hide();
+        $(".corps").hide();
+        $("footer").hide();
+        $("button").hide();
+    });
+});
